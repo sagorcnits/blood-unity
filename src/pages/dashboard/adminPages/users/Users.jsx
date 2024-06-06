5d1503542f5e14a98a43bb00f47efa7717358182
Sagor Hossain <sagor.cnits@gmail.com>
2024-06-06 23:10:42 +0600

    user page fitering added

--- src/pages/dashboard/adminPages/users/Users.jsx
+++ src/pages/dashboard/adminPages/users/Users.jsx
@@ -1,14 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { FaUsers } from "react-icons/fa";
+import { IoMdArrowDropdown } from "react-icons/io";
 import useAxiosPublice from "../../../../hooks/useAxiosPublice";
 
 const Users = () => {
   const axiosPublic = useAxiosPublice();
-
-  const { data: users = [],refetch } = useQuery({
-    queryKey: ["users"],
+  const [isFilter, setFilter] = useState(false);
+  const [users, setUsers] = useState([]);
+  const { data: userData = [], refetch } = useQuery({
+    queryKey: ["userData"],
     queryFn: async () => {
       const res = await axiosPublic.get("/users");
+      setUsers(res.data)
       return res.data;
     },
   });
@@ -19,18 +23,46 @@ const Users = () => {
       .put(`/users?role=${data}&id=${_id}`)
       .then((res) => {
         console.log(res.data);
-        refetch()
+        refetch();
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
+  //   handleFilter
+  const handleFilter = (value) => {
+    console.log(value);
+    if (value == "all") {
+      return setUsers(userData);
+    }
+    const filter = userData.filter((item) => item.status == value);
+    setUsers(filter);
+  };
+
   return (
     <div className="font-open-sans md:px-10 ">
-      <div className="text-[30px] mt-3 ml-2  flex gap-2 items-center">
-        <FaUsers className="text-[#27AE60]"></FaUsers>
-        <h1 className="font-bold">Users</h1>
+      <div className="flex justify-between items-center">
+        <div className="text-[30px] mt-3 ml-2  flex gap-2 items-center">
+          <FaUsers className="text-[#27AE60]"></FaUsers>
+          <h1 className="font-bold">Users</h1>
+        </div>
+        <div
+          onClick={() => setFilter(!isFilter)}
+          className="mr-3 bg-[#858686] w-[100px] px-3 py-2 text-white font-open-sans rounded-md relative cursor-pointer flex justify-between items-center"
+        >
+          <p>all</p>
+          <IoMdArrowDropdown
+            className={`${isFilter && "rotate-180"} duration-500`}
+          ></IoMdArrowDropdown>
+          {isFilter && (
+            <ul className="cursor-pointer py-1 bg-white card-shadow  font-open-sans px-2 rounded-md absolute -bottom-[110px] left-0 right-0 *:py-1 z-50 *:text-center *:bgHover *:text-black">
+              <li onClick={() => handleFilter("all")}>all</li>
+              <li onClick={() => handleFilter("active")}>active</li>
+              <li onClick={() => handleFilter("blocked")}>blocked</li>
+            </ul>
+          )}
+        </div>
       </div>
       <div className="p-2 mx-auto mt-3">
         <div className="bg-white rounded-md w-full">
@@ -47,7 +79,7 @@ const Users = () => {
               </tr>
             </thead>
             <tbody>
-              {users.slice(0, 4).map((user, id) => {
+              {users?.slice(0, 4).map((user, id) => {
                 const {
                   _id,
                   email,
@@ -59,6 +91,7 @@ const Users = () => {
                   status,
                   role,
                 } = user;
+                const statusBtn = status == "active" ? "block" : "unblock";
                 return (
                   <tr className="border-b dark:bg-gray-50 dark:border-gray-300 *:px-3 *:py-2 *:h-20">
                     <td>{id + 1}</td>
@@ -71,14 +104,20 @@ const Users = () => {
                       </div>
                     </td>
                     <td>{email}</td>
-                    <td>{role}</td>
-                    <td>{status}</td>
+                    <td className="font-semibold">{role}</td>
+                    <td
+                      className={
+                        status == "blocked" ? "text-darkRed" : "text-[#27AE60]"
+                      }
+                    >
+                      {status}
+                    </td>
                     <td>
                       <div className="dropdown dropdown-left dropdown-end">
                         <div tabIndex={0} className="cursor-pointer btn">
                           <svg
                             viewBox="0 0 24 24"
-                            className="w-4 h-4 fill-current"
+                            className="w-4 h-4 fill-current text-[#27AE60]"
                           >
                             <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z"></path>
                           </svg>
@@ -93,8 +132,8 @@ const Users = () => {
                           <li onClick={() => handleUser("volunteer", _id)}>
                             <a>Volunteer</a>
                           </li>
-                          <li onClick={() => handleUser("block", _id)}>
-                            <a>Block</a>
+                          <li onClick={() => handleUser(statusBtn, _id)}>
+                            <a>{statusBtn}</a>
                           </li>
                         </ul>
                       </div>
